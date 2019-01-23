fda084c30c094bbd2bc3917e0f589a39d1eff72d
Abhimanyu Gupta <abhimanyu.gupta07@gmail.com>
2019-01-23 13:28:54 +0000

    Updating S3 permissions for ecs task

--- main.tf
+++ main.tf
@@ -63,9 +63,40 @@ resource "aws_iam_role" "shuntingyard_task" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "task_managed" {
-  role       = "${aws_iam_role.shuntingyard_task.id}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+# resource "aws_iam_role_policy_attachment" "task_managed" {
+#   role       = "${aws_iam_role.shuntingyard_task.id}"
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+# }
+
+resource "aws_iam_role_policy" "s3_for_shuntingyard" {
+  name  = "s3"
+  role  = "${aws_iam_role.shuntingyard_task.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:DeleteObject",
+                "s3:DeleteObjectVersion",
+                "s3:Get*",
+                "s3:List*",
+                "s3:PutBucketLogging",
+                "s3:PutBucketNotification",
+                "s3:PutBucketVersioning",
+                "s3:PutObject",
+                "s3:PutObjectAcl",
+                "s3:PutObjectTagging",
+                "s3:PutObjectVersionAcl",
+                "s3:PutObjectVersionTagging"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy" "sqs_for_shuntingyard" {
