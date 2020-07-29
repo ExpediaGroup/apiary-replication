086c6b186a43c4b18133f7fea65442fe7bbc957d
Eduard <e.ahmatgareev@gmail.com>
2020-07-29 16:17:39 +0300

    moved blocks

--- sns.tf
+++ sns.tf
@@ -7,22 +7,3 @@
 resource "aws_sns_topic" "shuntingyard_ops_sns" {
   name = "${local.instance_alias}-operational-events"
 }
-
-data "template_file" "sqs_hive_metastore_sns_subscription_filter" {
-  template = <<EOF
-  {
-    "qualifiedTableName": [ $${tables_list} ]
-  }
-EOF
-
-  vars {
-    tables_list = "${join(",", formatlist("\"%s\"", var.selected_tables))}"
-  }
-}
-
-resource "aws_sns_topic_subscription" "sqs_hive_metastore_sns_subscription" {
-  topic_arn     = "${var.metastore_events_sns_topic}"
-  protocol      = "sqs"
-  endpoint      = "${aws_sqs_queue.shuntingyard_sqs_queue.arn}"
-  filter_policy = "${data.template_file.sqs_hive_metastore_sns_subscription_filter.rendered}"
-}
--- sqs.tf
+++ sqs.tf
@@ -34,3 +34,22 @@ resource "aws_sqs_queue_policy" "shuntingyard_sqs_queue_policy" {
 }
 POLICY
 }
+
+data "template_file" "sqs_hive_metastore_sns_subscription_filter" {
+  template = <<EOF
+  {
+    "qualifiedTableName": [ $${tables_list} ]
+  }
+EOF
+
+  vars {
+    tables_list = "${join(",", formatlist("\"%s\"", var.selected_tables))}"
+  }
+}
+
+resource "aws_sns_topic_subscription" "sqs_hive_metastore_sns_subscription" {
+  topic_arn     = "${var.metastore_events_sns_topic}"
+  protocol      = "sqs"
+  endpoint      = "${aws_sqs_queue.shuntingyard_sqs_queue.arn}"
+  filter_policy = "${data.template_file.sqs_hive_metastore_sns_subscription_filter.rendered}"
+}
