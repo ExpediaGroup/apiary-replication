20c2efecba817928e5a3bd77bf36a4ee42688659
Eduard <e.ahmatgareev@gmail.com>
2020-07-29 16:07:33 +0300

    fixes according comments

--- sns.tf
+++ sns.tf
@@ -7,3 +7,15 @@
 resource "aws_sns_topic" "shuntingyard_ops_sns" {
   name = "${local.instance_alias}-operational-events"
 }
+
+data "template_file" "sqs_hive_metastore_sns_subscription_filter" {
+  template = <<EOF
+  {
+    "qualifiedTableName": [ $${tables_list} ]
+  }
+EOF
+
+  vars {
+    tables_list = "${join(",", formatlist("\"%s\"", var.selected_tables))}"
+  }
+}
--- sqs.tf
+++ sqs.tf
@@ -7,7 +7,7 @@
 resource "aws_sqs_queue" "shuntingyard_sqs_queue" {
   name = "${local.instance_alias}-sqs-queue"
 
-  tags   = "${var.shuntingyard_tags}"
+  tags = "${var.shuntingyard_tags}"
 }
 
 resource "aws_sqs_queue_policy" "shuntingyard_sqs_queue_policy" {
@@ -40,16 +40,4 @@ resource "aws_sns_topic_subscription" "sqs_hive_metastore_sns_subscription" {
   protocol      = "sqs"
   endpoint      = "${aws_sqs_queue.shuntingyard_sqs_queue.arn}"
   filter_policy = "${data.template_file.sqs_hive_metastore_sns_subscription_filter.rendered}"
-}
-
-data "template_file" "sqs_hive_metastore_sns_subscription_filter" {
-  template = <<EOF
-  {
-    "qualifiedTableName": [ $${tables_list} ]
-  }
-EOF
-
-  vars {
-    tables_list = "${join(",", formatlist("\"%s\"", var.selected_tables))}"
-  }
 }
