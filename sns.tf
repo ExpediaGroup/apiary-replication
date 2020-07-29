8a6a58616d4b9a91219b9821c2702c5cc8cc947f
Eduard <e.ahmatgareev@gmail.com>
2020-07-29 16:12:35 +0300

    movede aws_sns_topic_subscription

--- sns.tf
+++ sns.tf
@@ -19,3 +19,10 @@ EOF
     tables_list = "${join(",", formatlist("\"%s\"", var.selected_tables))}"
   }
 }
+
+resource "aws_sns_topic_subscription" "sqs_hive_metastore_sns_subscription" {
+  topic_arn     = "${var.metastore_events_sns_topic}"
+  protocol      = "sqs"
+  endpoint      = "${aws_sqs_queue.shuntingyard_sqs_queue.arn}"
+  filter_policy = "${data.template_file.sqs_hive_metastore_sns_subscription_filter.rendered}"
+}
--- sqs.tf
+++ sqs.tf
@@ -34,10 +34,3 @@ resource "aws_sqs_queue_policy" "shuntingyard_sqs_queue_policy" {
 }
 POLICY
 }
-
-resource "aws_sns_topic_subscription" "sqs_hive_metastore_sns_subscription" {
-  topic_arn     = "${var.metastore_events_sns_topic}"
-  protocol      = "sqs"
-  endpoint      = "${aws_sqs_queue.shuntingyard_sqs_queue.arn}"
-  filter_policy = "${data.template_file.sqs_hive_metastore_sns_subscription_filter.rendered}"
-}
