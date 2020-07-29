4f4aaabaae557eaee06eb93d39f8e1f247387361
Eduard <e.ahmatgareev@gmail.com>
2020-07-29 15:50:58 +0300

    added tags for sqs

--- sqs.tf
+++ sqs.tf
@@ -6,6 +6,8 @@
 
 resource "aws_sqs_queue" "shuntingyard_sqs_queue" {
   name = "${local.instance_alias}-sqs-queue"
+
+  tags   = "${var.shuntingyard_tags}"
 }
 
 resource "aws_sqs_queue_policy" "shuntingyard_sqs_queue_policy" {
